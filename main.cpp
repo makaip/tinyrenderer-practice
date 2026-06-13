4be2d60379879804c1d782fa1b1f185e3f676a51
Makai P. <makaipindell@gmail.com>
2026-06-13 00:26:41 -0400

    move to func & start tri rasterization

--- main.cpp
+++ main.cpp
@@ -27,19 +27,68 @@ struct Triangle {
 };
 
 void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color);
+void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuffer, TGAColor color);
 std::tuple<int, int> project_vert(Vertex vert, int width, int height);
+void render_wireframe(std::ifstream &file, TGAImage &framebuffer, int width, int height);
 
 int main(int argc, char** argv) {
-    constexpr int width  = 800;
-    constexpr int height = 800;
+    constexpr int width  = 128;
+    constexpr int height = 128;
+
     TGAImage framebuffer(width, height, TGAImage::RGB);
+    // std::ifstream file("./obj/diablo3_pose/diablo3_pose.obj");
+    // render_wireframe(file, framebuffer, width, height);
 
-    int ax =  7, ay =  3;
-    int bx = 12, by = 37;
-    int cx = 62, cy = 53;
+    triangle(  7, 45, 35, 100, 45,  60, framebuffer, red);
+    triangle(120, 35, 90,   5, 45, 110, framebuffer, white);
+    triangle(115, 83, 80,  90, 85, 120, framebuffer, green);
 
-    std::ifstream file("./obj/diablo3_pose/diablo3_pose.obj");
+    framebuffer.write_tga_file("./framebuffer.tga", true, false);
+    return 0;
+}
 
+void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color) {
+    bool steep = std::abs(ax - bx) < std::abs(ay - by);
+
+    if (steep) {
+        std::swap(ax, ay);
+        std::swap(bx, by);
+    }
+
+    if (ax > bx) {
+        std::swap(ax, bx);
+        std::swap(ay, by);
+    }
+
+    float y = ay;
+    int ierror = 0;
+
+    for (int x = ax; x <= bx; x++) {
+        if (steep) {
+            framebuffer.set(y, x, color);
+        } else {
+            framebuffer.set(x, y, color);
+        }
+        
+        ierror += 2 * std::abs(ay - by);
+        y += (by > ay ? 1 : -1) * (ierror > bx - ax);
+        ierror -= 2 * (bx - ax) * (ierror > bx - ax);
+    }
+}
+
+void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuffer, TGAColor color) {
+    line(ax, ay, bx, by, framebuffer, color);
+    line(bx, by, cx, cy, framebuffer, color);
+    line(cx, cy, ax, ay, framebuffer, color);
+}
+
+std::tuple<int, int> project_vert(Vertex vert, int width, int height) {
+    int x = static_cast<int>((vert.x + 1.0)* width  / 2.0);
+    int y = static_cast<int>((vert.y + 1.0)* height / 2.0);
+    return {x, y};
+}
+
+void render_wireframe(std::ifstream &file, TGAImage &framebuffer, int width, int height) {
     std::vector<Vertex> vertices;
     std::vector<Triangle> triangles;
 
@@ -88,42 +137,4 @@ int main(int argc, char** argv) {
         line(t1x, t1y, t3x, t3y, framebuffer, red);
         line(t3x, t3y, t2x, t2y, framebuffer, red);
     }
-
-    framebuffer.write_tga_file("./framebuffer.tga", true, false);
-    return 0;
-}
-
-void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color) {
-    bool steep = std::abs(ax - bx) < std::abs(ay - by);
-
-    if (steep) {
-        std::swap(ax, ay);
-        std::swap(bx, by);
-    }
-
-    if (ax > bx) {
-        std::swap(ax, bx);
-        std::swap(ay, by);
-    }
-
-    float y = ay;
-    int ierror = 0;
-
-    for (int x = ax; x <= bx; x++) {
-        if (steep) {
-            framebuffer.set(y, x, color);
-        } else {
-            framebuffer.set(x, y, color);
-        }
-        
-        ierror += 2 * std::abs(ay - by);
-        y += (by > ay ? 1 : -1) * (ierror > bx - ax);
-        ierror -= 2 * (bx - ax) * (ierror > bx - ax);
-    }
-}
-
-std::tuple<int, int> project_vert(Vertex vert, int width, int height) {
-    int x = static_cast<int>((vert.x + 1.0)* width  / 2.0);
-    int y = static_cast<int>((vert.y + 1.0)* height / 2.0);
-    return {x, y};
 }
